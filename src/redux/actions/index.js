d343ce76c5a2511c8e8d45e5d0bfd52a9a50a34d
Diego Picoli <dgpicoli@gmail.com>
2022-09-08 17:02:48 -0300

    Resolve requisito 2

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,6 +1,16 @@
 export const USER_LOGIN = 'USER_LOGIN';
+export const USER_TOKEN = 'USER_TOKEN';
 
 export const userLogin = (state) => ({
   type: USER_LOGIN,
   payload: state,
 });
+
+export const addToken = (state) => ({
+  type: USER_TOKEN,
+  payload: state,
+});
+
+export const fetchToken = () => (dispatch) => fetch('https://opentdb.com/api_token.php?command=reques')
+  .then((response) => response.json())
+  .then((json) => dispatch(addToken(json)));
