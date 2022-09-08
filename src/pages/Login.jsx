d492e71c64e1cfc66958dfeabba838465b4c2323
Pedro Ayres <pedroayresb@gmail.com>
2022-09-08 15:51:39 -0300

    Realiza requisito 1

---
+++ src/pages/Login.jsx
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+// import md5 from 'crypto-js/md5';
+// import { userLogin } from '../redux/actions';
+
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isDisabled: true,
+      email: '',
+      name: '',
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => this.validateForm());
+  };
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    const { history } = this.props;
+    // const { email, name } = this.state;
+    // const hash = md5(email).toString();
+    history.push('/game');
+  };
+
+  validateForm() {
+    const { email, name } = this.state;
+    const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+    const minNameLength = 1;
+    const isEmailValid = emailRegex.test(email);
+    const isNameValid = name.length >= minNameLength;
+    this.setState({
+      isDisabled: !(isEmailValid && isNameValid),
+    });
+  }
+
+  render() {
+    const { isDisabled } = this.state;
+    return (
+      <div>
+        <h1>Login</h1>
+        <form>
+          <label htmlFor="email">
+            Email:
+            <input
+              type="email"
+              name="email"
+              id="email"
+              data-testid="input-gravatar-email"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="name">
+            Name:
+            <input
+              type="text"
+              name="name"
+              id="name"
+              data-testid="input-player-name"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button
+            type="submit"
+            data-testid="btn-play"
+            disabled={ isDisabled }
+            onClick={ this.handleSubmit }
+          >
+            Play
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+Login.propTypes = {
+  // dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default connect()(Login);
---
+++ src/redux/actions/index.js
@@ -0,0 +1,6 @@
+export const USER_LOGIN = 'USER_LOGIN';
+
+export const userLogin = (state) => ({
+  type: USER_LOGIN,
+  payload: state,
+});
