db159b0c4468fd7242c751217da5cdcfbdd93ab6
Diego Picoli <dgpicoli@gmail.com>
2022-09-12 16:38:15 -0300

    Resolve requisito 18 e 19

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,11 +1,62 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { getRanking } from '../services/localstorage';
 
 class Ranking extends Component {
   render() {
+    const num1 = -1;
+    const ranking = getRanking().sort((a, b) => {
+      if (a.score > b.score) {
+        return num1;
+      }
+      return true;
+    });
     return (
-      <h1 data-testid="ranking-title">Ranking</h1>
+      <div>
+        <h1 data-testid="ranking-title">Ranking</h1>
+        <button
+          data-testid="btn-go-home"
+          type="submit"
+          onClick={ () => {
+            const { history } = this.props;
+            history.push('/');
+          } }
+        >
+          Home
+        </button>
+        <ol>
+          { ranking.map((elemento, index) => (
+            <li key={ index }>
+              <img
+                src={ elemento.picture }
+                alt="avatar"
+              />
+              <p
+                data-testid={ `player-name-${index}` }
+                name="name"
+                value="name"
+              >
+                { elemento.name }
+              </p>
+              <p
+                name="currency"
+                value="score"
+                data-testid={ `player-score-${index}` }
+              >
+                { elemento.score }
+              </p>
+            </li>
+          ))}
+        </ol>
+      </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Ranking;
--- src/services/localstorage.js
+++ src/services/localstorage.js
@@ -1,11 +1,11 @@
 const RANKING_KEY = 'ranking';
 
-if (!localStorage.getItem(RANKING_KEY)) {
-  localStorage.setItem(RANKING_KEY, JSON.stringify([]));
-}
-
-export const getRanking = () => JSON
-  .parse(localStorage.getItem(RANKING_KEY));
+export const getRanking = () => {
+  if (!localStorage.getItem(RANKING_KEY)) {
+    localStorage.setItem(RANKING_KEY, JSON.stringify([]));
+  }
+  return JSON.parse(localStorage.getItem(RANKING_KEY));
+};
 
 const setRanking = (ranking) => localStorage
   .setItem(RANKING_KEY, JSON.stringify(ranking));
